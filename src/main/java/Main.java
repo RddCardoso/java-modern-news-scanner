3abe00bda59d6b6227302d3b71a427b3b81bc4ed
Rui Cardoso <rui_d_a_cardoso@hotmail.com>
2026-05-08 09:39:48 +0100

    Adicionados comentários à lógica de Streams

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,10 +12,17 @@ public class Main {
            System.out.println("Ligado a: " + doc.title());
 
            // 2. Extrair notícias e transformar em objectos 'Artigo' usando Streams
+            // 2.1. O "Íman": Procura no HTML todos os links (<a>) que estão dentro de elementos com a classe .titleline
             List<Artigo> noticias = doc.select(".titleline > a").stream()
+
+                    // 2.2. O "Transfomador": Pega em cada elemento HTML bruto e cria um objecto 'Artigo' (Título + Link)
                     .map(elemento -> new Artigo(elemento.text(), elemento.absUrl("href")))
+
+                    // 2.3. O "Filtro": Só deixa passar os artigos cujo título (em minúsculas) contenha "java OU "ai"
                     .filter(artigo -> artigo.titulo().toLowerCase().contains("java") ||
                                     artigo.titulo().toLowerCase().contains("ai"))
+
+                    // 2.4. O "Empacotador": Recolhe todos os que passaram no filtro e guarda-os numa lista final
                     .toList(); // Java 16+ simplificou o .collect(Collectors.toList())
 
             // 3. Mostrar os resultados

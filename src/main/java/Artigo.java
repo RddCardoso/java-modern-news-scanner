988c9939a82febfbdd22ba4e927cc08ed2c7cf30
Rui Cardoso <rui_d_a_cardoso@hotmail.com>
2026-05-12 22:45:43 +0100

    feat: infrastructure and database setup complete

--- src/main/java/Artigo.java
+++ src/main/java/Artigo.java
@@ -1,1 +1,49 @@
-public record Artigo(String titulo, String link) {}
+import jakarta.persistence.*;
+
+@Entity // Define que esta classe é uma tabela no banco de dados
+@Table(name= "artigos")
+public  class Artigo{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // O Postgres gera o ID sozinho (1, 2, 3...)
+    private Long id;
+
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT") // TEXT permite textos longos (links ou descrições)
+    private String link;
+
+    // Construtor  vazio obrigatório para o JPA
+    public Artigo() {}
+
+    public Artigo(String titulo, String link) {
+        this.titulo = titulo;
+        this.link = link;
+    }
+
+    // Getters e Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ src/main/java/ArtigoRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
+    // Aqui já tens acesso a .save(), .findAll(), .delete(), etc.
+}
